b1cdcdb4081058479bb4f2d344aa222362f255bd
saTrent <trent01@gmail.com>
2014-03-15 16:28:26 -0500

    set up jquery animation

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var express = require('express');
 
 var app = express();
 
+app.use('/', express.static(__dirname));
 app.use("/css", express.static(__dirname + '/css'));
 app.use('/jQuery', express.static(__dirname + '/jQuery'));
 
