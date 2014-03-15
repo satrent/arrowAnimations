73365fba7c84fd7bccde83a676e050651b3820c0
saTrent <trent01@gmail.com>
2014-03-14 23:41:16 -0500

    first commit

---
+++ server.js
@@ -0,0 +1,12 @@
+var http = require('http');
+var express = require('express');
+
+var app = express();
+
+app.use("/css", express.static(__dirname + '/css'));
+app.use('/jQuery', express.static(__dirname + '/jQuery'));
+
+
+http.createServer(app).listen(9008, function() {
+  console.log('web server listening on port 9008');
+});
